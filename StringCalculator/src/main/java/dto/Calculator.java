22f2c819a573d6865cb1a0e85a3f383a65784a18
betterfly <betterfly4202@gmail.com>
2018-02-25 15:36:34 +0900

    upload string calculator

---
+++ StringCalculator/src/main/java/dto/Calculator.java
@@ -0,0 +1,10 @@
+package dto;
+
+/**
+ * Created by betterFLY on 2018. 2. 22.
+ * Github : http://github.com/betterfly88
+ */
+
+public interface Calculator {
+    double Calculator(double a, double b);
+}
