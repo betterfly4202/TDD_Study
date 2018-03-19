a2efcfd270d91aee6e36ce789a2b7db9779f4fe7
betterfly <betterfly4202@gmail.com>
2018-03-20 01:14:18 +0900

    wishlist books

--- RacingGame/src/main/java/main/RacingGameController.java
+++ RacingGame/src/main/java/main/RacingGameController.java
@@ -11,16 +11,22 @@ import java.util.Scanner;
 
 public class RacingGameController {
     public static void main(String [] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("경주에 참여할 자동차 수를 입력해주세요 : ");
-        int cars = sc.nextInt();
-
-        Scanner tt = new Scanner(System.in);
-        System.out.print("진행할 라운드를 입력해주세요 : ");
-        int times = tt.nextInt();
-
+        int cars = getCars();
+        int times = getTimes();
         RacingGameImpl racingGame = new RacingGameImpl(cars, times);
         racingGame.start();
+    }
 
+    public static int getCars(){
+        Scanner cars = new Scanner(System.in);
+        System.out.print("경주에 참여할 자동차 수를 입력해주세요 : ");
+        return cars.nextInt();
+    }
+
+    public static int getTimes(){
+        Scanner times = new Scanner(System.in);
+        System.out.print("진행할 라운드를 입력해주세요 : ");
+
+        return times.nextInt();
     }
 }
