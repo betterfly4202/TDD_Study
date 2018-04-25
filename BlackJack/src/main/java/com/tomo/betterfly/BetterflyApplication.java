fc3e282cb99d3aa1bd3091837bc7c95748cd4f4f
betterfly <betterfly4202@gmail.com>
2018-04-26 00:07:17 +0900

    start BlackJack

---
+++ BlackJack/src/main/java/com/tomo/betterfly/BetterflyApplication.java
@@ -0,0 +1,12 @@
+package com.tomo.betterfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterflyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterflyApplication.class, args);
+	}
+}
