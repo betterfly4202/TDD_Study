72c99d8ed863aa026e6d15190f7b535aa8e31b71
betterfly <betterfly4202@gmail.com>
2018-05-18 00:49:16 +0900

    change to cardnumberentity type

--- BlackJack/src/main/java/blackjack/entity/CardNumberEntity.java
+++ BlackJack/src/main/java/blackjack/entity/CardNumberEntity.java
@@ -1,37 +1,37 @@
 package blackjack.entity;
 
-import java.util.Arrays;
-
 /**
  * Created by betterFLY on 2018. 5. 11.
  * Github : http://github.com/betterfly88
  */
 
 public enum CardNumberEntity {
-    ACE("ACE"),
-    TWO("2"),
-    THREE("3"),
-    FOUR("4"),
-    FIVE("5"),
-    SIX("6"),
-    SEVEN("7"),
-    EIGHT("8"),
-    NINE("9"),
-    TEN("10"),
-    JACK("J"),
-    QUEEN("Q"),
-    KING("K");
+    ACE(1) {
+        public int isAce(boolean selected) {
+            return selected == true ? 11 : 1;
+        }
+    },
+    TWO(2),
+    THREE(3),
+    FOUR(4),
+    FIVE(5),
+    SIX(6),
+    SEVEN(7),
+    EIGHT(8),
+    NINE(9),
+    TEN(10),
+    JACK(10),
+    QUEEN(10),
+    KING(10);
 
-    final private String number;
+    final private int cardNumber;
 
-    CardNumberEntity(String number) {
-        this.number = number;
+    CardNumberEntity(int cardNumber) {
+
+        this.cardNumber = cardNumber;
     }
 
-    public String getNumber() {
-        return number;
+    public int getCardNumber(){
+        return cardNumber;
     }
-
-
-
 }
