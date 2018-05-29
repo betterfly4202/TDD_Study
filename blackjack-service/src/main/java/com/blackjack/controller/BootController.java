9f48019147b72a0f5850d3c65b536faff9d2f53e
betterfly <betterfly4202@gmail.com>
2018-05-30 00:46:30 +0900

    no message

--- blackjack-service/src/main/java/com/blackjack/controller/BootController.java
+++ blackjack-service/src/main/java/com/blackjack/controller/BootController.java
@@ -1,7 +1,14 @@
 package com.blackjack.controller;
 
+import com.blackjack.entity.Card;
+import com.blackjack.entity.CardDeck;
+import com.blackjack.entity.Player;
+import com.blackjack.entity.PlayerEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by betterFLY on 2018. 5. 27.
@@ -12,7 +19,28 @@ import org.springframework.web.bind.annotation.RestController;
 public class BootController {
 
     @GetMapping("/main")
-    public String index() {
+    public String index(Model model){
+        model.addAttribute("player",playerCardDeck());
+        model.addAttribute("dealer",dealerCardDeck());
         return "main";
+    }
+
+
+    public List<Card> playerCardDeck(){
+        CardDeck cardDeck = new CardDeck();
+        cardDeck.setCardDeckList();
+        Player player = new Player(PlayerEntity.USER, 2000);
+        List<Card> userCard = player.getUserCardDeckList();
+
+        return userCard;
+    }
+
+    public List<Card> dealerCardDeck(){
+        CardDeck cardDeck = new CardDeck();
+        cardDeck.setCardDeckList();
+        Player dealer = new Player(PlayerEntity.DEALER);
+        List<Card> dealerCard = dealer.getUserCardDeckList();
+
+        return dealerCard;
     }
 }
