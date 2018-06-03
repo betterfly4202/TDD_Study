db9387fb9092530ff92fff20eee068743289ef20
betterfly <betterfly4202@gmail.com>
2018-06-03 23:29:34 +0900

    web config

---
+++ blackjack-service/src/main/java/com/blackjack/Application.java
@@ -0,0 +1,17 @@
+package com.blackjack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by betterFLY on 2018. 6. 3.
+ * Github : http://github.com/betterfly88
+ */
+
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
