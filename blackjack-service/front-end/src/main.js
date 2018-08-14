ed12b9952eeaf57a13838522111cbaa2dd893235
betterfly <betterfly4202@gmail.com>
2018-08-15 01:28:44 +0900

    no message

--- blackjack-service/front-end/src/main.js
+++ blackjack-service/front-end/src/main.js
@@ -3,13 +3,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+const myApi = axios.create({
+  baseURL: 'http://localhost:8080',
+  timeout: 10000,
+  withCredentials: true,
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  }
+});
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  myApi,
   components: { App },
   template: '<App/>'
 })
