cd7dde375e7e0d36b66be1236d15f940ab7e24d6
이충일 <betterfly4202@gmail.com>
2018-12-09 18:00:06 +0900

    git commit -v sample!!!

--- GameOfLadders/src/main/java/com/tdd/ladders/Users.java
+++
@@ -1,9 +0,0 @@
-package com.tdd.ladders;
-
-/**
- * Created by betterfly
- * Date : 2018.12.05
- */
-public class Users {
-
-}
---
+++ GameOfLadders/src/main/java/com/tdd/ladders/item/Ladder.java
@@ -0,0 +1,8 @@
+package com.tdd.ladders.item;
+
+/**
+ * Created by betterfly
+ * Date : 2018.12.09
+ */
+public class Ladder {
+}
